e0209e2f1e20a8b2ddc954a0f68e86035b81261b
Joel Jakubovic <16978710+jdjakub@users.noreply.github.com>
2025-08-06 16:38:28 +0200

    JS objs from boxes+arrows

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# self-raising-diagrams
-Vector graphics as a generalisation of source code (i.e. text that raises itself into a dynamic program)
+# Self-Raising Diagrams
+A text file is a static medium. Source code is self-raising text: the static text raises itself into a dynamic run-time environment (usually unvisualised).
+
+A vector graphics diagram is a static medium. A self-raising diagram is a generalisation of source code: it raises itself into a dynamic run-time environment, this time with an obvious visualisation.
+
+We take advantage of *existing* drawing tools to draw the diagrams, just as we take advantage of *existing* text editors to write code (even to write our own custom syntaxes). This sidesteps the classic trap of having to implement your own (crappy) custom drawing software just to get the precise notational semantics that you want. Imagine if you always had to build your own text editor just to use your domain-specific syntax!
+
+Outsource the drawing to drawing experts, and encode dynamic behaviour in the diagram somehow (just like source code). The shapes get drawn and placed in software *optimised* for drawing; all the rest is performed by multiple compilation passes from diagram to diagram, or interpretation of the diagram in a run-time environment. This is the part that is unique to us as notational engineers - *not* coding event handlers for line rubberbanding for the umpteenth time.
+
+It is futile to expect to reinvent Adobe Illustrator in Squeak Smalltalk in order to draw beautiful GUIs. Just use Adobe Illustrator itself to make a beautiful schematic, with embedded instructions for responding to change, and load this into your favourite environment as if it were source code.
+
+In this project, I draw in [Mathcha.io](https://www.mathcha.io/editor) and output to (horrible) SVG. The SVG shall raise itself into a dynamic web app in the web browser.
+
+# Object vs Meta Graphics
+Some parts of the diagram are object-level: they're part of the intended output. Other parts (perhaps in a designated meta-colour, like blue) are instructions to the next transformation pass to alter the diagram in some way (add stuff, remove stuff, etc). Sorta like macros.
+
+# Current instructions
+Currently, we recognise a simple visual notation of boxes, arrows, and text labels. Arrows are labelled by the closest text element. Each box means a JS object, and each arrow means a property, with the "obvious" semantics on that.
+
+Insert the following into `diagram.svg`, below the top svg element:
+
+```html
+<script href="./misc.js"></script>
+```
+
+Open `diagram.svg` in Firefox. In the Ctrl-Shift-I console, call `main()` and see if `objs` encodes the correct relationships.
--- misc.js
+++ misc.js
@@ -1,14 +1,77 @@
+/* ### UTILITIES ### */
+
+// For entering the debugger in a JS console statement. Add it as a dummy param
+// or use it to wrap a param in a function call so it evaluates first.
+// E.g: I want to step through the execution of `foo(1, 2, 3)`.
+// So I put: `foo(1, 2, DEBUG(3))` or `foo(1, 2, 3, DEBUG())`.
+DEBUG = (x) => { debugger; return x; };
+// `last([1,2,3])` = 3, `last([1,2,3], 2)` = 2
+last = (arr, n) => arr[arr.length-(n || 1)];
+// Interpose anywhere in an expression to transparently probe its value.
+// E.g. `foo(1, bar(x)*baz(y))` - I wonder what the 2nd argument is.
+// So I put: `foo(1, log(bar(x)*baz(y)))`
+log = (...args) => { console.log(...args); return last(args); };
+
+attr_single = (elem, key, val_or_func) => {
+  let old;
+  if (key === 'textContent') old = elem.textContent;
+  else old = elem.getAttribute(key);
+
+  let value = typeof(val_or_func) === 'function' ? val_or_func(old) : val_or_func;
+  if (key === 'textContent') elem.textContent = value;
+  else if (value !== undefined) elem.setAttribute(key, value);
+
+  return old;
+};
+
+// e.g. attr(rect, {stroke_width: 5, stroke: 'red'})
+//      attr(rect, 'stroke', 'red')
+//      attr(rect, 'height', h => h+32)
+//      attr(rect, {fill: 'orange', height: h => h+32})
+attr = (elem, key_or_dict, val_or_nothing) => {
+  if (typeof(key_or_dict) === 'string') {
+    let key = key_or_dict;
+    let value = val_or_nothing;
+    return attr_single(elem, key, value);
+  } else {
+    let dict = key_or_dict;
+    for (let [k,v_or_f] of Object.entries(dict)) {
+      let key = k.replace('_','-');
+      attr_single(elem, key, v_or_f);
+    }
+  }
+}
+
+nums = (arr) => arr.map(x => +x);
+attrs = (el, ...keys) => keys.map(k => attr(el, k));
+props = (o,  ...keys) => keys.map(k => o[k]);
+
+svg_parent = document.body; // Default parent for new SVG elements
+
+create_element = (tag, attrs, parent, namespace) => {
+  let elem = document.createElementNS(namespace, tag);
+  if (attrs !== undefined) attr(elem, attrs);
+  if (parent === undefined) parent = svg_parent;
+  parent.appendChild(elem);
+  return elem;
+};
+
+// e.g. rect = svgel('rect', {x: 5, y: 5, width: 5, height: 5}, svg)
+svgel = (tag, attrs, parent) => create_element(tag, attrs, parent, 'http://www.w3.org/2000/svg');
+
+/* ### MAIN ### */
+
 // Mathcha SVG outputs simple shapes as paths and multiline text as separate text elements...
 // Gotta recognise basic shapes. Computer Vector Vision
 
 extractPathCmds = function(pathElt) {
-    const d = pathElt.getAttribute('d')
-    const clauses = d.split(' ').filter(c => c.length > 0);
-    const cmds = clauses.map(c => [c[0], c.substr(1).split(',').map(s => Number.parseFloat(s))]);
-    const opcodes = cmds.map(c => c[0]).join('');
-    const xs = cmds.map(c => c[1][0]);
-    const ys = cmds.map(c => c[1][1]);
-    return [opcodes,xs,ys];
+  const d = pathElt.getAttribute('d')
+  const clauses = d.split(' ').filter(c => c.length > 0);
+  const cmds = clauses.map(c => [c[0], c.substr(1).split(',').map(s => Number.parseFloat(s))]);
+  const opcodes = cmds.map(c => c[0]).join('');
+  const xs = cmds.map(c => c[1][0]);
+  const ys = cmds.map(c => c[1][1]);
+  return [opcodes,xs,ys];
 }
 
 /*
@@ -18,15 +81,15 @@ extractPathCmds = function(pathElt) {
   width="rx-lx" height="by-ty" />
 */
 extractRect = function(rectPathElt) {
-    const [opcodes,xs,ys] = extractPathCmds(rectPathElt);
-    if (opcodes !== 'MLLLZ') return;
-    if (xs[0] !== xs[3]) return;
-    if (ys[0] !== ys[1]) return;
-    if (xs[1] !== xs[2]) return;
-    if (ys[2] !== ys[3]) return;
-    
-    const [lx,ty,rx,by] = [xs[0],ys[0],xs[1],ys[2]];
-    return [lx,ty,rx-lx,by-ty];
+  const [opcodes,xs,ys] = extractPathCmds(rectPathElt);
+  if (opcodes !== 'MLLLZ') return;
+  if (xs[0] !== xs[3]) return;
+  if (ys[0] !== ys[1]) return;
+  if (xs[1] !== xs[2]) return;
+  if (ys[2] !== ys[3]) return;
+  
+  const [lx,ty,rx,by] = [xs[0],ys[0],xs[1],ys[2]];
+  return [lx,ty,rx-lx,by-ty];
 }
 
 /*
@@ -38,9 +101,64 @@ arrow's origin is [xo,yo]
 arrow's target is [xt,yt]
 */
 extractArrow = function(arrowGroupElt) {
-    const shaftPathElt = arrowGroupElt.querySelector('path.real');
-    const [opcodes,xs,ys] = extractPathCmds(shaftPathElt);
-    if (opcodes !== 'ML') return;
-    const [xo,yo,xt,yt] = [xs[0],ys[0],xs[1],ys[1]];
-    return {origin: [xo,yo], target: [xt,yt]};
+  const shaftPathElt = arrowGroupElt.querySelector('path.real');
+  const [opcodes,xs,ys] = extractPathCmds(shaftPathElt);
+  if (opcodes !== 'ML') return;
+  const [xo,yo,xt,yt] = [xs[0],ys[0],xs[1],ys[1]];
+  return {origin: [xo,yo], target: [xt,yt]};
+}
+
+explodeRect = function(r) {
+  const [lx,rx,ty,by] = [r.x,r.x+r.width,r.y,r.y+r.height];
+  return [[lx,ty],[rx,ty],[rx,by],[lx,by]];
+}
+
+dist2 = ([x,y],[z,w]) => (z-x)**2 + (w-y)**2;
+
+findClosestTextElt = function(origin) {
+  const ts = document.querySelectorAll('text');
+  const bboxes = Array.from(ts, t => [t, explodeRect(t.getBBox())]);
+  const points = bboxes.flatMap(([t,pts]) => pts.map(p => ({
+    element: t, coords: p, dist2: dist2(origin,p)
+  })));
+  const closestPt = points.reduce((min,pt) => min.dist2 < pt.dist2 ? min : pt);
+  return closestPt;
+}
+
+main = function(){
+
+arrows = Array.from(
+  document.querySelectorAll('.arrow-line'),
+  g => ({element: g, ...extractArrow(g)})
+);
+
+telts = arrows.map(a => findClosestTextElt(a.origin));
+
+rects = Array.from(document.querySelectorAll('path.real'))
+  .filter(r => !r.classList.contains('connection'))
+  .map(path => ({element: path, params: extractRect(path), obj: {}}));
+
+CONTAINS_PT_EPSILON = 3;
+containsPt = function([lx,ty,w,h],[x,y]) {
+  const [relx,rely] = [x-lx,y-ty];
+  const [propx,propy] = [relx/w,rely/h];
+  const ex = CONTAINS_PT_EPSILON/w;
+  const ey = CONTAINS_PT_EPSILON/h;
+  return -ex < propx && propx < 1+ex && -ey < propy && propy < 1+ey;
+}
+
+findRectContainingPt = coords => rects.find(r => containsPt(r.params,coords));
+
+o_rects = arrows.map(a => findRectContainingPt(a.origin));
+t_rects = arrows.map(a => findRectContainingPt(a.target));
+
+telts.forEach((telt,i) => {
+  const label = telt.element.textContent;
+  const origin = o_rects[i].obj;
+  const target = t_rects[i].obj;
+  origin[label] = target;  
+});
+
+objs = rects.map(r => r.obj);
+return objs;
 }
