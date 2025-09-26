f69adaa6e2dc2e4aa1413939abe4429993c11b60
Joel Jakubovic <16978710+jdjakub@users.noreply.github.com>
2025-09-26 13:49:35 +0200

    bitsTable done for PAINT

--- bitsTable.js
+++ bitsTable.js
@@ -112,8 +112,9 @@ pass.makeTableCols = function() {
           const mathchaWrapper = svgel('g');
           // Make cell slightly smaller than the row, so that if it is the only cell, it doesn't
           // have exactly the same rect as the row, causing cyclic containment shenanigans...
+          // It's essential that we keep the widths the same, else assignBitRanges will break...
           const cell = svgel('rect',
-            {x: prevCol_x+1, y: row_y1+1, width: cut_x - prevCol_x -1, height: row.extent[1] -1},
+            {x: prevCol_x, y: row_y1+1, width: cut_x - prevCol_x, height: row.extent[1] -1},
           mathchaWrapper);
           cell.id = row.dom.id + '-col' + i;
           cell.classList.add('is-cell');
@@ -124,8 +125,104 @@ pass.makeTableCols = function() {
   });
 }
 
-pass.splitHeaderText = function() {
+isHeaderSpec = function({string}) {
+  const nums = string.trim().split(' ')
+    .filter(s => s.length > 0).map(x => Number.parseInt(x, 10));
+  return nums.every(x => !Number.isNaN(x));
+}
 
+getBitRange = function(str) {
+  const nums = str.trim().split(' ')
+    .filter(s => s.length > 0).map(x => Number.parseInt(x, 10));
+  return [nums[0], last(nums)+1];
+}
+
+leftToRight = (a, b) => a.getBBox().x - b.getBBox().x;
+
+// Requires: makeTableCols
+pass.readHeaderText = function() {
+  log('Reading header bit specification.')
+  const rows = all('.is-row');
+  const headerPara = getParagraphs().filter(isHeaderSpec)[0]; // fragile
+  const headerRow = headerPara.dom.parentElement;
+  headerRow.classList.add('is-header');
+  const headerText = headerPara.dom.querySelector('text');
+
+  // SMELL: duped
+  let verLines = getLines().filter(l => l.type === 'vertical');
+  const rect = getRect(byId(verLines[0].dom.dataset.containedIn));
+  const xs = verLines.map(l => ({line: l, key: l.originPt[0]}));
+  xs.sort((x1, x2) => x1.key - x2.key);
+  verLines = xs.map(x => x.line);
+
+  const headerCells = Array.from(headerRow.querySelectorAll('.is-cell'));
+  headerCells.sort(leftToRight);
+  let textCut_x = headerPara.dom.getBBox().x;
+  let textCutStart_i = 0;
+  const textLen = headerText.textContent.length;
+  headerCells.forEach(c => {
+    const r = getRect(c);
+    const target_x = r.topRight[0];
+    let textCut_i = textCutStart_i;
+    while (textCut_x < target_x && textCut_i < textLen) {
+      textCut_x += headerText.getExtentOfChar(textCut_i).width;
+      textCut_i++;
+    }
+    const substring = headerText.textContent.substring(textCutStart_i, textCut_i);
+    c.dataset.bitRange = vtoa(getBitRange(substring));
+    textCutStart_i = textCut_i;
+  });
+}
+
+// Requires: readHeaderText
+pass.assignBitRanges = function() {
+  log('Assigning bit ranges.');
+  const headerCells = all('.is-header .is-cell');
+  headerCells.sort(leftToRight);
+
+  const rows = all('.is-row').filter(r => !r.parentElement.classList.contains('is-header'));
+  rows.forEach(row => {
+    const cells = Array.from(row.parentElement.querySelectorAll('.is-cell'));
+    cells.sort(leftToRight);
+    let headerStart_i = 0;
+    cells.forEach(cell => {
+      const myWidth = cell.getBBox().width;
+      let headerEnd_i = headerStart_i;
+      let totalWidth = 0;
+      // -3 tolerance for misaligned ver lines; TODO pass.alignColLines()
+      while (totalWidth < myWidth - 3) {
+        totalWidth += headerCells[headerEnd_i].getBBox().width;
+        headerEnd_i++;
+      }
+      const headerStartCell = headerCells[headerStart_i];
+      const [startBit] = atov(headerStartCell.dataset.bitRange);
+      let endBit = null;
+      if (headerEnd_i < headerCells.length) {
+        const headerEndCell = headerCells[headerEnd_i];
+        [endBit] = atov(headerEndCell.dataset.bitRange);
+      } else {
+        [_,endBit] = atov(last(headerCells).dataset.bitRange);
+      }
+      cell.dataset.bitRange = vtoa([startBit,endBit]);
+      headerStart_i = headerEnd_i;
+    });
+  });
+}
+
+// Requires: assignBitRanges
+pass.generateStructData = function() {
+  const data = [];
+  const rows = all('.is-row').filter(r => !r.parentElement.classList.contains('is-header'));
+  rows.forEach(row => {
+    const cells = Array.from(row.parentElement.querySelectorAll('.is-cell'));
+    cells.sort(leftToRight);
+    cells.forEach(cell => {
+      const t = cell.parentElement.querySelector('text');
+      const [startBit,endBit] = atov(cell.dataset.bitRange);
+      data.push([t.textContent, endBit-startBit]);
+    });
+  });
+  return data;
 }
 
 function doAll() {
@@ -137,4 +234,10 @@ function doAll() {
   pass.idLabels();
   pass.annotateParagraphs();
   pass.annotateAllContainments();
+  pass.treeifyContainments();
+  pass.makeDOMReflectContainmentTree();
+  pass.readHeaderText();
+  pass.assignBitRanges();
+  structData = pass.generateStructData();
+  return structData;
 }
