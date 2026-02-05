0d98eb05facdeb0728bfb894b981e1891935e30d
Joel Jakubovic <16978710+jdjakub@users.noreply.github.com>
2026-02-05 19:29:32 +0100

    BoxGraph bootstrap proof-of-concept

---
+++ boxGraph-bootstrap.js
@@ -0,0 +1,544 @@
+// All BoxGraph-related code in here has been moved into boxGraph-bootstrap.svg
+
+vtables = { byTag: {}, };
+
+// We want to be able to send Smalltalk-style messages to SVG DOM nodes
+// e.g. send(rectElem, 'doSomething:', blah1, 'with:', blah2, 'and:', blah3)
+// = sendNoKw(rectElem, 'doSomething:with:and:', blah1, blah2, blah3)
+// NOTE: JSTalk macro syntactic sugar
+//   send(rectElem, 'doSomething:', blah1, 'with:', blah2, 'and:', blah3)
+// should expand (jstalk2js.pl) into the above send().
+// BTW: the VS Code extension.js auto-replaces [[ -> ⟦ and ]] -> ⟧ as you type
+// If the sugar isn't working for you, just work with the verbose .js output file
+
+send = function(recv, ...pairs) {
+  if (pairs.length === 1) return sendNoKw(recv, ...pairs); // Unary message
+  if (pairs.length % 2 !== 0) throw ['Odd args:', pairs]; // Binary / keyword message
+  let selector = [];
+  const args = [];
+  for (let i=0; i<pairs.length; i += 2) {
+    selector.push(pairs[i]);
+    args.push(pairs[i+1]);
+  }
+  selector = selector.join('');
+  return sendNoKw(recv, selector, ...args);
+}
+
+// TODO: supersends
+sendNoKw = function(recv, selector, ...args) {
+  let vtable;
+  if (recv.tagName) vtable = vtables.byTag[recv.tagName];
+  else if (recv instanceof Array && recv.length === 2) vtable = vtables.point;
+  else if (typeof recv.vtable === 'string') vtable = vtables[recv.vtable];
+  else vtable = recv.vtable;
+  let method;
+  do {
+    method = vtable[selector];
+    vtable = vtable._parent; // i.e. superclass
+  } while (!method && vtable);
+  if (!method && !vtable) {
+    if (selector === 'doesNotUnderstand:')
+      throw [recv," didn't understand: ",selector,...args];
+    else
+      return sendNoKw(recv, 'doesNotUnderstand:', [selector, ...args]);
+  }
+  return method(recv, ...args);
+}
+
+vtables.point = {
+  ['distanceTo:']: (self, other) => {
+    if (other instanceof Array && other.length === 2) {
+      return Math.sqrt(dist2(self, other));
+    } else { // Assumes < domNode
+      const closest = closest_line_seg_to_pt(self, send(other, 'lineSegments'));
+      return Math.sqrt(distance2_pt_to_line_seg(self, ...closest));
+    }
+  },
+  ['insideWhichShapes:']: (self, shapes) =>
+    shapes.filter(s => send(s, 'containsPt:', self)).sort(inTopToBottomOrder),
+  ['pickFrom:']: (self, shapes) => {
+    shapes = send(self, 'insideWhichShapes:', shapes);
+    if (shapes.length > 0) return shapes[0];
+    else return null;
+  },
+  ['closestPointOn:']: (self, shape) => send(shape, 'closestPtToPt:', self),
+};
+
+nextId = 1;
+
+vtables.domNode = {
+  _parent: null,
+
+  ['id']: (self) => {
+    // TODO: pull in + exec any red boxes on demand!?
+    let id = attr(self, 'id');
+    if (!id) {
+      const prefix = send(self, 'idPrefix');
+      id = prefix+(nextId++);
+      attr(self, 'id', id);
+    }
+    return id;
+  },
+  ['idPrefix']: (self) => {
+    let ret = {
+      'path': 'e', 'polygon': 'p', 'polyline': 'pl',
+      'text': 't', 'circle': 'c', 'rect': 'r', 'line': 'l'
+    }[self.tagName];
+    if (!ret) ret = 'e';
+    return ret;
+  },
+  ['containsPt:']: (self, [x,y]) => {
+    const bb = self.getBBox();
+    const [l,t,r,b] = [bb.x,bb.y,bb.x+bb.width,bb.y+bb.height];
+    return l<=x && x<=r && t<=y && y<=b;
+  },
+  ['encloses:']: (self, other) => {
+    const otherVs = send(other, 'vertices'); // SMELL convex polys only
+    for (const v of otherVs) {
+      if (!send(self, 'containsPt:', v)) return false;
+    }
+    return true;
+  },
+  ['vertices']: (self) => {
+    const bb = self.getBBox(); // SMELL duped
+    const [l,t,r,b] = [bb.x,bb.y,bb.x+bb.width,bb.y+bb.height];
+    return [ [l,t], [r,t], [r,b], [l,b] ];
+  },
+  ['isClosed']: (self) => true,
+  ['lineSegments']: (self) => explode_poly_segs(send(self, 'vertices'), send(self, 'isClosed')),
+  ['distanceTo:']: (self, other) => {
+    if (other instanceof Array) return send(other, 'distanceTo:', self);
+    return Math.sqrt(distance2_line_segs_to_segs(send(self, 'lineSegments'), send(other, 'lineSegments')));
+  },
+  ['findTightestContainerIn:']: (self, elems) => { // -> containedIn
+    // MUCH nicer code than annotateAllContainments plus treeifyContainments
+    // I want to find my least / tightest container
+    let container = null; // Infinitely big initial container
+    elems.filter(other => self !== other).forEach(other => {
+      const rivalExists = send(other, 'encloses:', self);
+      if (rivalExists) {
+        const rival = other;
+        // If the rival sits within my current tightest container, rival is tighter
+        if (container === null || send(container, 'encloses:', rival)) container = rival;
+      }
+    });
+    if (container) {
+      addSetAttr(container.dataset, 'contains', send(self, 'id'));
+      self.dataset.containedIn = send(container, 'id');
+    }
+  },
+  ['specialize']: (self) => null,
+  ['localRoot']: (self) => self,
+  ['reroot']: (self) => { // Given: containedIn
+    let soonToBeParent = byId(self.dataset.containedIn);
+    const parentRoot = send(soonToBeParent, 'localRoot');
+    const myRoot = send(self, 'localRoot');
+    parentRoot.appendChild(myRoot);
+  },
+  ['atPoint:pickFrom:']: (self, pt, shapes) => send(pt, 'pickFrom:', shapes.filter(s => s !== self)),
+  ['connectors']: (self) => {
+    if (self.dataset.connectors) return setAttrToArray(self.dataset.connectors).map(byId);
+    return [];
+  }
+};
+
+vtables.byTag['path'] = {
+  _parent: vtables.domNode,
+
+  ['isClosed']: (self) => {
+    return attr(self, 'd').toUpperCase().trimEnd().endsWith('Z');
+  },
+  ['isCurved']: (self) => { // NB: technically could be degen bezier polygon
+    const d = attr(self, 'd').toUpperCase();
+    return d.includes('Q') || d.includes('C') || d.includes('S') || d.includes('T') || d.includes('A');
+  },
+  ['commands']: (self) => {
+    const d = attr(self, 'd');
+    return parsePath(d);
+  },
+  ['pointAtFrac:']: (self, frac /* 0 to 1 */) => {
+    const total = self.getTotalLength();
+    const pt = self.getPointAtLength(frac * total);
+    return [pt.x, pt.y];
+  },
+  ['closestPtToPt:']: (self, pt) => closestPointOnPath(self, pt),
+  ['containsPt:']: (self, pt) => {
+    if (!send(self, 'isClosed')) {
+      const {point, d2} = send(pt, 'closestPointOn:', self);
+      if (d2 < 4) return true;
+      return false;
+    }
+    throw "Should be overridden";
+  },
+  ['encloses:']: (self, other) => {
+    if (!send(self, 'isClosed')) return false;
+    else return vtables.domNode['encloses:'](self, other); // HACK supersend
+  },
+  ['specialize']: (self) => {
+    let newTag = null;
+    if (!send(self, 'isCurved')) { // => Polygon | Polyline
+      const polyPts = polyFromPath(send(self, 'commands')).map(v => v.join(',')).join(' ');
+      attr(self, 'points', polyPts);
+      newTag = send(self, 'isClosed')? 'polygon' : 'polyline';
+    } else {
+      // SMELL: duped from normalizeCircles
+      const params = extractCircle(self);
+      if (params) { // Circle
+        attr(self, params);
+        newTag = 'circle';
+      }
+    }
+    if (newTag) {
+      self = replaceTag(self, newTag);
+      self.removeAttribute('d');
+      return self;
+    }
+    return null;
+  },
+  ['localRoot']: (self) => self.parentElement,
+  ['endpoints']: (self) => {
+    if (send(self, 'isClosed')) return [];
+    return [ send(self, 'pointAtFrac:', 0), send(self, 'pointAtFrac:', 1) ];
+  },
+  ['connections']: (self) => {
+    if (self.dataset.connects) return self.dataset.connects.split(' ').map(byId);
+    if (self.dataset.origin && self.dataset.target
+      && self.dataset.origin !== 'nil' && self.dataset.target !== 'nil')
+      return [self.dataset.origin, self.dataset.target].map(byId);
+    const endpoints = send(self, 'endpoints');
+    const connections = endpoints.map(pt => send(self, 'atPoint:', pt, 'pickFrom:', Object.values(everything)));
+    self.dataset.connects = connections.map(c => send(c, 'id')).join(' ');
+    const myId = send(self, 'id');
+    connections.forEach(e => addSetAttr(e.dataset, 'connectors', myId))
+    return connections;
+  },
+};
+
+vtables.byTag['polyline'] = {
+  _parent: vtables.byTag['path'],
+
+  ['vertices']: (self) => {
+    return attr(self, 'points').trim().split(' ').map(v => v.split(',').map(Number));
+  },
+  ['isClosed']: () => false,
+  ['isCurved']: () => false,
+  ['commands']: (self) => {
+    let vs = send(self, 'vertices');
+    vs = vs.map(v => ['L', v]);
+    vs[0][0] = 'M';
+    return vs;
+  },
+  ['encloses:']: () => false,
+  ['specialize']: (self) => {
+    let newTag = null;
+    const points = send(self, 'vertices');
+    if (points.length === 2) {
+      attr(self, {x1: points[0][0], y1: points[0][1], x2: points[1][0], y2: points[1][1]});
+      newTag = 'line';
+    }
+    if (newTag) {
+      self = replaceTag(self, newTag);
+      self.removeAttribute('points');
+      return self;
+    }
+    return null;
+  },
+};
+
+vtables.byTag['line'] = {
+  _parent: vtables.byTag['polyline'],
+
+  ['vertices']: (self) => [attrs(self, 'x1', 'y1'), attrs(self, 'x2', 'y2')].map(pt => pt.map(parseFloat)),
+  ['specialize']: () => null,
+};
+
+vtables.byTag['polygon'] = {
+  _parent: vtables.byTag['polyline'],
+
+  ['isClosed']: () => true,
+  ['specialize']: (self) => {
+    let newTag = null;
+    const vertices = send(self, 'vertices');
+    if (vertices.length === 4) {
+      const [tl,tr,br,bl] = vertices;
+      // NB: requires axis-aligned and clockwise starting from top-left
+      if (tl[0] === bl[0] && tr[0] === br[0] && tl[1] === tr[1] && bl[1] === br[1]) {
+        const params = {x: tl[0], y: tl[1], width: tr[0]-tl[0], height: bl[1]-tl[1]};
+        attr(self, params);
+        newTag = 'rect';
+      }
+    }
+    if (newTag) {
+      self = replaceTag(self, newTag);
+      self.removeAttribute('points');
+      return self;
+    }
+    return null;
+  },
+  ['containsPt:']: (self, pt) => {
+    const vs = send(self, 'vertices');
+    return isPointInPolygon(pt, vs);
+  },
+  ['encloses:']: vtables.domNode['encloses:'], // HACK super?
+}
+
+MAGIC_RED = 'rgb(208, 2, 27)';
+vtables.byTag['rect'] = {
+  _parent: vtables.byTag['polygon'],
+
+  ['vertices']: (self) => {
+    const [x,y,w,h] = attrs(self, 'x', 'y', 'width', 'height').map(Number);
+    return [ [x,y], [x+w,y], [x+w,y+h], [x,y+h] ];
+  },
+  ['specialize']: () => null,
+  ['parseAsExecutable']: (self) => {
+    // Check for executable boxes
+    if (self.style.stroke === MAGIC_RED) {
+      const lroot = send(self, 'localRoot');
+      const paras = Array.from(lroot.querySelectorAll('.is-paragraph'));
+      let done = true;
+      paras.forEach(p => {
+        if (!p.classList.contains('done')) done = false;
+        const str = p.dataset.string;
+        const lines = str.split('\n');
+        if (lines.length === 1 && str.startsWith('#')) {
+          p.classList.add('sets-id'); return true;
+        } else if (lines.every(l => l.startsWith('.'))) {
+          p.classList.add('adds-class'); return true;
+        } else {
+          p.classList.add('is-code'); return true;
+        }
+      });
+      if (done) self.classList.add('done');
+    }
+    return false;
+  },
+}
+
+vtables.byTag['circle'] = {
+  _parent: vtables.byTag['path'],
+
+  ['isClosed']: () => true,
+  ['isCurved']: () => true,
+  ['vertices']: (self) => {
+    // Sigh ... approximate circle as octagon
+    const [cx,cy,r] = attrs(self, 'cx', 'cy', 'r').map(Number);
+    const n = 8;
+    const theta = Math.PI*2/n;
+    const vs = [];
+    for (let i=0; i<n; i++) {
+      const itheta = i*theta;
+      const [x,y] = [Math.cos(itheta), Math.sin(itheta)];
+      const avg_r = r*(1+1/Math.cos(theta/2))/2; // 1/2 between inner and outer polygon
+      vs.push(vadd([cx,cy], vmul(avg_r, [x,y])));
+    }
+    return vs;
+  },
+  ['specialize']: () => null,
+  ['containsPt:']: (self, pt) => {
+    const [cx,cy,r] = attrs(self, 'cx', 'cy', 'r').map(Number);
+    const pt_from_c = vsub(pt, [cx,cy]);
+    return vdot(pt_from_c,pt_from_c) < r*r;
+  },
+}
+
+vtables.byTag['text'] = {
+  _parent: vtables.domNode,
+}
+
+vtables.byTag['g'] = {
+  _parent: vtables.domNode,
+
+  ['parseAsParagraph']: (self) => {
+    if (self.children.length === 0) return false;
+    for (let child of self.children) {
+      if (child.tagName !== 'g') return false;
+      for (let gchild of child.children) {
+        if (gchild.tagName !== 'g') return false;
+        if (gchild.firstChild.tagName !== 'text') return false;
+      }
+    }
+    // TODO: maybe sanity check they're in y-order
+    const lines = Array.from(self.children).map(line_g => {
+      const runs = Array.from(line_g.children).map(run_g =>
+        run_g.firstChild.textContent
+      );
+      return runs.join(' ');
+    });
+    self.dataset.string = lines.join('\n');
+    if (self.children.length > 1) self.classList.add('is-multiline');
+    self.classList.add('is-paragraph');
+    return true;
+  },
+  ['idPrefix']: (self) => {
+    if (self.classList.contains('is-paragraph')) return 'par';
+    else return 'g';
+  },
+  //SMELL domNode>>localRoot wrong for line/run <g>'s
+  ['execute']: (self) => {
+    const str = self.dataset.string;
+    const lines = str.split('\n');
+    const parent_g = self.parentElement;
+    const red_box = parent_g.firstChild; // SMELL nondeterminism
+    let targets = [parent_g.parentElement.firstChild]; // SMELL nondeterminism
+    const conns = send(red_box, 'connectors');
+    if (conns.length > 0) // Find the connections that aren't the red box itself
+      targets = conns.map(c => send(c, 'connections').filter(x => x !== red_box)[0]);
+    if (self.classList.contains('sets-id')) {
+      // #myId para sets target id=myId and self-deletes
+      // SMELL what if new ID already in use
+      if (targets.length > 1) throw [self, 'sets-id needs exactly 1 target'];
+      const target = targets[0];
+      const newId = str.substring(1);
+      const clash = byId(newId);
+      if (clash) clash.id = target.id;
+      target.id = newId;
+      self.classList.add('done');
+    } else if (self.classList.contains('adds-class')) {
+      // .myClass line adds myClass to container and self-deletes
+      lines.forEach(l => {
+        targets.forEach(target => target.classList.add(l.substring(1)));
+      });
+      self.classList.add('done');
+    } else if (self.classList.contains('is-code')) {
+      eval(str); // >:D
+      self.classList.add('done');
+    }
+    conns.forEach(c => c.classList.add('done'));
+  }
+}
+
+// Of a connector (open path) c, we must be able to ask:
+// c endpoints -> [p1, p2]
+// c connections -> [el1, el2]
+// c origin/target -> el or null
+
+// === MATHCHA ARROW SUPPORT ===
+
+/* In DOMMeta terms, a Mathcha arrow is recognised something like:
+  * g .arrow-line {
+      path .connection .real :shaft ,
+      ( g { path :head1 } ) ? ,
+      ( g { path :head2 } ) ?
+    }
+  But remember: we also have "fat arrows". Generally, any shape can
+  be parsed as a connector: just determine the two endpoints.
+  However, such "generalised connector" behaviour should probably not
+  live in Path.
+*/
+
+vtables.byTag['path']['target'] = (self) => {
+  if (self.dataset.target) return byId(self.dataset.target);
+  const endpoints = send(self, 'endpoints');
+  const lroot = send(self, 'localRoot');
+  const arrowheads = Array.from(lroot.querySelectorAll('g'));
+  let originPt = null;
+  let target = nilElem;
+  if (arrowheads.length === 1) {
+    const m = arrowheads[0].transform.baseVal[0].matrix;
+    const targetPt = [m.e, m.f];
+    const [d0,d1] = [dist2(targetPt,endpoints[0]), dist2(targetPt,endpoints[1])];
+    const originIndex = d0 < d1 ? 1 : 0;
+    self.dataset.originIndex = originIndex;
+    originPt = endpoints[originIndex];
+    target = send(self, 'atPoint:', targetPt, 'pickFrom:', Object.values(everything));
+  }
+  self.dataset.origin = 'nil';
+  self.dataset.target = 'nil';
+  if (originPt) {
+    const origin = send(self, 'atPoint:', originPt, 'pickFrom:', Object.values(everything));
+    if (origin) {
+      self.dataset.origin = send(origin, 'id');
+      addSetAttr(origin.dataset, 'connectors', send(self, 'id'));
+    }
+  }
+  if (target) {
+    self.dataset.target = send(target, 'id');
+    addSetAttr(target.dataset, 'connectors', send(self, 'id'));
+  }
+  return target;
+}
+
+vtables.byTag['path']['origin'] = (self) => {
+  if (self.dataset.origin) return byId(self.dataset.origin);
+  send(self, 'target');
+  return byId(self.dataset.origin);
+}
+
+vtables.byTag['path']['isDirected'] = (self) => {
+  send(self, 'target');
+  return !isNaN(parseInt(self.dataset.originIndex));
+}
+
+vtables.byTag['path']['originPt'] = (self) => {
+  if (send(self, 'isDirected')) {
+    const endpoints = send(self, 'endpoints');
+    return endpoints[+self.dataset.originIndex];
+  }
+}
+
+vtables.byTag['path']['targetPt'] = (self) => {
+  if (send(self, 'isDirected')) {
+    const endpoints = send(self, 'endpoints');
+    return endpoints[1 - +self.dataset.originIndex];
+  }
+}
+
+everything = {};
+function init() {
+  // First, ensure "nil" exists
+  nilElem = svgel('g', {id: 'nil'});
+
+  let elems = all('path.real, polygon');
+  elems = elems.concat(all('g').filter(g => send(g, 'parseAsParagraph')));
+  elems.forEach((el) => {
+    let newEl = el;
+    let max_iter = 10;
+    do { // max specialize
+      el = newEl;
+      newEl = send(el, 'specialize');
+      max_iter--;
+    } while (max_iter > 0 && newEl);
+    everything[ send(el, 'id') ] = el;
+  });
+
+  elems = Object.values(everything);
+  elems.forEach(el => send(el, 'findTightestContainerIn:', elems));
+  
+  // MUCH nicer than makeDOMReflectContainmentTree
+  all('[data-contained-in]').forEach(child => {
+    send(child, 'reroot');
+    delete child.dataset.containedIn;
+  });
+  all('[data-contains]').forEach(e => {
+    delete e.dataset.contains;
+  });
+
+  const codeConnectors = Object.values(everything)
+    .filter(x => !send(x, 'isClosed'))
+    .filter(x => x.style.stroke === MAGIC_RED);
+  codeConnectors.forEach(c => send(c, 'connections'));
+
+  // TODO: need to save code to execute until ALL processing done
+  // even with further formats. I.e. delay execution to point where
+  // the diagram "means" that which was intended by the user
+  all('rect').forEach(r => send(r, 'parseAsExecutable'));
+  all('.adds-class').forEach(p => send(p, 'execute')); // must occur before sets-id
+  all('.sets-id').forEach(p => send(p, 'execute'));
+  all('.is-code').forEach(p => send(p, 'execute'));
+  all('rect').forEach(r => send(r, 'parseAsExecutable'));
+  all('.done').forEach(e => e.remove()); // WARNING: connections[*].connectors will be stale
+
+  return elems.length;
+}
+
+/*
+In order for a generic shape (1D/2D) to claim a label, we want to use
+a context-specific shape as proxy. E.g. for boxGraph, a 1D arrow claims
+the closest label to its *origin point* (0D). Meanwhile, a 2D box claims
+the closest label to its entire shape. Afterwards, max distances or further
+restrictions are applied.
+
+Don't prematurely commit to the specific DOM.
+*/
+
